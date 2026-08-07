bc146f63106e9c661b9e34c380d5b11cfb9c3d80
psrujana541-jpg <psrujana541@gmail.com>
2026-08-08 00:21:35 +0530

    Update SOCKET_URL to use environment variable

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,9 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Use same host or fallback to port 3001 in dev
-const SOCKET_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
-  ? 'http://localhost:3001' 
-  : window.location.origin;
+const SOCKET_URL = import.meta.env.VISIT_SERVER_URL || 'http:/localhost:3001'; 
 
 export const socket = io(SOCKET_URL, {
   autoConnect: true,
